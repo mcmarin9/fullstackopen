2ce5a7e80130dca5d5840f72f8536144a2366c1f
mcmarin9 <mcmarin9@alu.ucam.edu>
2024-10-29 23:18:59 +0100

    part3 update before deploying

---
+++ part3/example/index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const cors = require('cors')
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+//middleware
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(cors())
+
+
+// funcion
+const generateId = () => {
+  const maxId =
+    notes.length > 0
+      ? Math.max(...notes.map((n) => n.id)) // el map devuelve un array con todas las notas, pero no se le puede hacer max. se transforma en numeros individuales con ...
+      : 0;
+  return maxId + 1;
+};
+
+//rutas
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  note ? response.json(note) : response.status(404).end();
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id); // no es que que la borre en sí, es que crea un nuevo array sin ese y se reasigna a notes
+
+  response.status(204).end; // 204 no content
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+//
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
