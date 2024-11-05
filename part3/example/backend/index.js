796c8a9039419837710e097f8e3b0f1970b9315a
mcmarin9 <mcmarin9@alu.ucam.edu>
2024-11-05 21:48:44 +0100

    uibuild

--- part3/example/backend/index.js
+++ part3/example/backend/index.js
@@ -19,24 +19,6 @@ let notes = [
   }
 ]
 
-const mongoose = require('mongoose')
-
-const password = process.argv[2]
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -63,9 +45,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+  response.json(notes)
 })
 
 const generateId = () => {
