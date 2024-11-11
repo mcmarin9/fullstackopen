1f41c17588c973b1bbe42e71efc2c41f70425139
mcmarin9 <mcmarin9@alu.ucam.edu>
2024-11-11 23:25:47 +0100

    part 4 start

---
+++ part4/example/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react" 
+import Note from "./components/Note" 
+import Notification from "./components/Notification" 
+import Footer from "./components/Footer" 
+import noteService from "./services/notes" 
+
+const App = () => {
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState("") 
+  const [showAll, setShowAll] = useState(true) 
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => { // estos then serían para el método del servicio
+        setNotes(initialNotes) 
+      }) 
+  }, [])  // argumento 1: la función a ejecutar, argumento 2: array de dependencias, si alguna dependencia cambia, se vuelve a ejecutar. si está vacío una vez solo al iniciar.
+
+  console.log("render", notes.length, "notes") 
+
+  const addNote = (event) => {
+    event.preventDefault() 
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    } 
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote)) 
+        setNewNote("") 
+      }) 
+  } 
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)  // busca la nota a cambiar
+    const changedNote = { ...note, important: !note.important }  // crea una copia de la nota y le cambia la importancia
+    // changedNote = { id: 1, content: "Learn React", important: false } coge lo que hay anterior
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))  // map crea el nuevo array de notas, si no coincide se copia tal cual, si el id es el mismo se pone el actualizado. digamos que va copiando y comprobando uno a uno.
+      })
+      .catch(error => {
+        setErrorMessage (
+          `the note '${note.content}' was already deleted from server `
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      }) 
+  } 
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value) 
+  } 
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)  // Si showAll es true, las muestra todas, si no las filtra
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage}/>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"} {/* esto solo mostrará texto */}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer/>
+    </div>
+  ) 
+} 
+
+export default App 
