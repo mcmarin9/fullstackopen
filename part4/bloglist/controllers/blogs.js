31dad01a9346336266aac41a9eb3f8382d69601b
mcmarin9 <mcmarin9@alu.ucam.edu>
2024-11-19 22:11:18 +0100

    part4-1 fixed

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,28 +1,27 @@
-const blogsRouter = require('express').Router();
-const Blog = require('../models/blogs');
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs')
 
-blogsRouter.get('/', async (request, response,) => {
+blogsRouter.get('/', (request, response) => {
   Blog.find({}).then(blogs => {
     response.json(blogs)
   })
-});
+})
 
 blogsRouter.post('/', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-  });
+  })
 
   blog.save()
     .then(savedBlog => {
-      response.status(201).json(savedBlog);
+      response.json(savedBlog)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
-
-module.exports = blogsRouter;
+module.exports = blogsRouter
