697ac515a5127b97051fb715889804a49a130c22
mcmarin9 <mcmarin9@alu.ucam.edu>
2025-03-20 00:14:44 +0100

    idk

--- part5/example/src/App.jsx
+++ part5/example/src/App.jsx
@@ -1,94 +1,134 @@
-import { useState, useEffect } from "react" 
-import Note from "./components/Note" 
-import Notification from "./components/Notification" 
-import Footer from "./components/Footer" 
-import noteService from "./services/notes" 
+import { useState, useEffect } from "react";
+import Note from "./components/Note";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
+import noteService from "./services/notes";
+import loginService from "./services/login";
 
 const App = () => {
-  const [notes, setNotes] = useState([]) 
-  const [newNote, setNewNote] = useState("") 
-  const [showAll, setShowAll] = useState(true) 
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => { // estos then serían para el método del servicio
-        setNotes(initialNotes) 
-      }) 
-  }, [])  // argumento 1: la función a ejecutar, argumento 2: array de dependencias, si alguna dependencia cambia, se vuelve a ejecutar. si está vacío una vez solo al iniciar.
+    noteService.getAll().then((initialNotes) => {
+      // estos then serían para el método del servicio
+      setNotes(initialNotes);
+    });
+  }, []); // argumento 1: la función a ejecutar, argumento 2: array de dependencias, si alguna dependencia cambia, se vuelve a ejecutar. si está vacío una vez solo al iniciar.
 
-  console.log("render", notes.length, "notes") 
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
-    event.preventDefault() 
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-    } 
+    };
 
-    noteService
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote)) 
-        setNewNote("") 
-      }) 
-  } 
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
 
   const toggleImportanceOf = (id) => {
-    const note = notes.find((n) => n.id === id)  // busca la nota a cambiar
-    const changedNote = { ...note, important: !note.important }  // crea una copia de la nota y le cambia la importancia
+    const note = notes.find((n) => n.id === id); // busca la nota a cambiar
+    const changedNote = { ...note, important: !note.important }; // crea una copia de la nota y le cambia la importancia
     // changedNote = { id: 1, content: "Learn React", important: false } coge lo que hay anterior
 
     noteService
       .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))  // map crea el nuevo array de notas, si no coincide se copia tal cual, si el id es el mismo se pone el actualizado. digamos que va copiando y comprobando uno a uno.
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote))); // map crea el nuevo array de notas, si no coincide se copia tal cual, si el id es el mismo se pone el actualizado. digamos que va copiando y comprobando uno a uno.
       })
-      .catch(error => {
-        setErrorMessage (
+      .catch((error) => {
+        setErrorMessage(
           `the note '${note.content}' was already deleted from server `
-        )
+        );
         setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      }) 
-  } 
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
 
   const handleNoteChange = (event) => {
-    setNewNote(event.target.value) 
-  } 
+    setNewNote(event.target.value);
+  };
 
-  const handleLogin = (event) => {
-    event.preventDefault()
-    console.log(('logging in with ', username, password))
-  }
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important)  // Si showAll es true, las muestra todas, si no las filtra
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username{" "}
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        pasasword{" "}
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  );
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important); // Si showAll es true, las muestra todas, si no las filtra
 
   return (
     <div>
       <h1>Notes</h1>
-      <Notification message={errorMessage}/>
+      <Notification message={errorMessage} />
 
-      <form onSubmit={handleLogin}>
-      <div>
-        username <input type="text" value={username}
-        name="Username" onChange={({ target }) => setUsername(target.value)}/>
-      </div>
-      <div>
-        pasasword <input type="pasasword" value={pasasword}
-        name="Password" onChange={({ target }) => setPassword(target.value)}/>
-      </div>
-      <button type="submit">login</button>
-      </form>
+      {/* {user === null && loginForm()}
+      {user !== null && noteForm()} */}
 
-
-
-
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
@@ -104,13 +144,10 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
-      <Footer/>
-    </div>
-  ) 
-} 
 
-export default App 
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
